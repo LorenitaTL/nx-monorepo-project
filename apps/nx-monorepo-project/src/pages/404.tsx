b5f9b6e2b8e4c0de031a35113d721188a8cb0381
Lorena Trujillo <lore7096@gmail.com>
2024-10-22 17:34:16 -0600

    add error page

---
+++ apps/nx-monorepo-project/src/pages/404.tsx
@@ -0,0 +1,10 @@
+export default function Custom404() {
+  return (
+    <div className="">
+      <h2 className="">Algo salió mal.</h2>
+      <p className="">
+        Si crees que es un error, por favor repórtalo para solucionarlo.
+      </p>
+    </div>
+  );
+}
